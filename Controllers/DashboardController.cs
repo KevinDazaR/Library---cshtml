1c18685d1b300b18671d92cfff8f84703ffca5c6
KevinDazaR <61398857+KevinDazaR@users.noreply.github.com>
2024-03-23 18:05:50 -0500

    Add files via upload

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+
+}
